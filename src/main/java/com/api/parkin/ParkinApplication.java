5a026f2ac64407565eafcfa7be9ec0135da1b9be
BRAZ DINIZ <90735705+braz2021@users.noreply.github.com>
2023-06-13 21:08:26 -0300

    primeiro commit

---
+++ src/main/java/com/api/parkin/ParkinApplication.java
@@ -0,0 +1,13 @@
+package com.api.parkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/parkin/ParkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
